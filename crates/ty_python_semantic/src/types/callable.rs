9b402ec19eae4949d359198d06df8ab308f09e30
Charlie Marsh <charlie.r.marsh@gmail.com>
2026-04-12 20:48:20 -0400

    [ty] Preserve generic functools.partial signatures

--- crates/ty_python_semantic/src/types/callable.rs
+++ crates/ty_python_semantic/src/types/callable.rs
@@ -266,9 +266,10 @@ impl<'db> Type<'db> {
             | Type::TypeGuard(_)
             | Type::TypedDict(_) => None,
 
-            Type::KnownInstance(KnownInstanceType::FunctoolsPartial(partial)) => {
-                Some(CallableTypes::one(partial.partial(db)))
-            }
+            Type::KnownInstance(
+                KnownInstanceType::FunctoolsPartial(partial)
+                | KnownInstanceType::FunctoolsPartialCall(partial),
+            ) => Some(CallableTypes::one(partial.partial(db))),
 
             Type::Intersection(intersection) => {
                 intersection
