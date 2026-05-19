fc2e4d0465c153162514756cf9185ebf71c20460
Dhruv Manilawala <dhruvmanila@gmail.com>
2026-05-19 20:21:07 +0530

    [ty] Add initial TypeVarTuple support

--- crates/ty_ide/src/hover.rs
+++ crates/ty_ide/src/hover.rs
@@ -3562,10 +3562,10 @@ def function():
         );
 
         assert_snapshot!(test.hover(), @"
-        @Todo
+        AB@Alias3 (covariant)
         ---------------------------------------------
         ```python
-        @Todo
+        AB@Alias3 (covariant)
         ```
         ---------------------------------------------
         info[hover]: Hovered content is
@@ -3693,10 +3693,10 @@ def function():
         );
 
         assert_snapshot!(test.hover(), @"
-        @Todo
+        Ts@Alias (covariant)
         ---------------------------------------------
         ```python
-        @Todo
+        Ts@Alias (covariant)
         ```
         ---------------------------------------------
         info[hover]: Hovered content is
